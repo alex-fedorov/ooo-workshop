069e195632a6dc0d211519d357a540457c237591
Alexey Fedorov <alexey.fedorov@wimdu.com>
2014-11-12 17:40:17 +0100

    Better node through path

--- node.rb
+++ node.rb
@@ -1,17 +1,63 @@
 class Edge < Struct.new(:node, :costs)
   private :node, :costs
 
-  # TODO Cost strategy
-  def _cost(*args, &blk)
-    node._cost(*args, &blk) + blk[self]
+  def self.total_cost(links)
+    links.inject(0) { |total, link| link.add_to(total) }
+  end
+
+  def add_to(total)
+    total + costs.min
+  end
+
+  def _path_to(*args)
+    node._path_to(*args) << self
+  end
+end
+
+class Path
+  def self.cheapest(paths)
+    paths.inject { |champion, path| path.cheaper?(champion) ? path : champion } || NoPath.new
+  end
+
+  def cheaper?(other)
+    self.cost < other.cost
+  end
+
+  def cost
+    Edge.total_cost(links)
   end
 
   def hop_count
-    1
+    links.count
   end
 
-  def path_cost
-    costs.min
+  def <<(link)
+    links << link
+    self
+  end
+
+  private
+
+  def links
+    @_links ||= []
+  end
+end
+
+class NoPath
+  def <<(link)
+    self
+  end
+
+  def cheaper?(other)
+    false
+  end
+
+  def cost
+    Float::INFINITY
+  end
+
+  def hop_count
+    Float::INFINITY
   end
 end
 
@@ -27,50 +73,47 @@ class Node < Struct.new(:name)
   end
 
   def can_reach?(other)
-    _cost(other, &:hop_count) < Unreachable
+    !(NoPath === _path_to(other))
   end
 
   def hop_count(other)
-    fails_for_unreachable(_cost(other, &:hop_count))
+    path_to(other).hop_count
   end
 
   def path_cost(other)
-    fails_for_unreachable(_cost(other, &:path_cost))
+    path_to(other).cost
+  end
+
+  def path_to(other)
+    fails_for_no_path(_path_to(other))
   end
 
   def inspect
     "#{name}"
   end
 
-  def _cost(other, visited_nodes = [], &blk)
-    return 0 if self.eql?(other)
-    return Unreachable if visited_nodes.include?(self)
-    min(edge_costs(other, visited_nodes_with_self(visited_nodes), &blk))
+  def _path_to(other, visited_nodes = [])
+    return Path.new if self.eql?(other)
+    return NoPath.new if visited_nodes.include?(self)
+    Path.cheapest(edge_paths(other, visited_nodes_with_self(visited_nodes)))
   end
 
   private
 
   def visited_nodes_with_self(visited_nodes)
-    # NOTE with dup it is a backtrack search
-    # to turn it to depth-first search remove dup
-    # but this will make you get not least cost result
     visited_nodes.dup << self
   end
 
-  def edge_costs(other, visited_nodes, &blk)
-    edges.map { |edge| edge._cost(other, visited_nodes, &blk) }
+  def edge_paths(other, visited_nodes)
+    edges.map { |edge| edge._path_to(other, visited_nodes) }
   end
 
   def edges
     @_edges ||= []
   end
 
-  def fails_for_unreachable(cost)
-    raise UnreachableNodeError if cost >= Unreachable
-    cost
-  end
-
-  def min(costs)
-    costs.min || Unreachable
+  def fails_for_no_path(path)
+    raise UnreachableNodeError if NoPath === path
+    path
   end
 end
--- node_spec.rb
+++ node_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Node' do
   before do
     b.edge(a, 6)
     b.edge(c, 7).edge(d, 3, 1).edge(e, 2).edge(b, 4).edge(f, 5)
-    c.edge(e, 1)
+    c.edge(e, 8)
   end
 
   it { expect(b).to be_can_reach(e) }
@@ -25,5 +25,16 @@ RSpec.describe 'Node' do
   it { expect{ b.hop_count(g) }.to raise_error(Node::UnreachableNodeError) }
 
   it { expect(b.path_cost(f)).to eq(5) }
-  it { expect(b.path_cost(e)).to eq(8) }
+  it { expect(b.path_cost(e)).to eq(10) }
+
+  it { expect(b.path_to(f).hop_count).to eq(1) }
+  it { expect(b.path_to(f).cost).to eq(5) }
+
+  it { expect(b.path_to(b).hop_count).to eq(0) }
+  it { expect(b.path_to(b).cost).to eq(0) }
+
+  it { expect(b.path_to(e).hop_count).to eq(3) }
+  it { expect(b.path_to(e).cost).to eq(10) }
+
+  it { expect{ b.path_to(g) }.to raise_error(Node::UnreachableNodeError) }
 end
